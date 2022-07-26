29d1e451cdddc57c0dfd0fd22229584f3bd946bc
Seynabou12 <dioneseynabou0@gmail.com>
2022-07-26 20:09:41 +0000

    intégration de template

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,41 @@
+<div class="nav-header">
+    <a href="index.html" class="brand-logo">
+
+        <img src="/images/" alt="" srcset="" width="70" height="60"   class="">
+        
+     
+    </a>
+    <div class="nav-control">
+        <div class="hamburger">
+            <span class="line"></span><span class="line"></span><span class="line"></span>
+        </div>
+    </div>
+</div>
+
+<div class="header">
+    <div class="header-content">
+        <nav class="navbar navbar-expand">
+            <div class="collapse navbar-collapse justify-content-between">
+                <div class="header-left">
+                    <div class="dashboard_bar">
+                        Dashbord Admin
+                    </div>
+                </div>
+                <ul class="navbar-nav header-right">
+                    <li class="nav-item">
+                        <div class="input-group search-area">
+                            <input type="text" class="form-control" placeholder="Search here...">
+                            <span class="input-group-text"><a href="javascript:void(0)"><i
+                                        class="flaticon-381-search-2"></i></a></span>
+                        </div>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a href="javascript:void(0);" class="btn  d-sm-inline-block d-none" style="background-color: #fd7e14; color: white;">Portail Captif<i class="las la-signal ms-3 scale5"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+      
+    </div>
+</div>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,110 @@
+<div class="dlabnav">
+    <div class="dlabnav-scroll">
+        <ul class="metismenu" id="menu">
+            <li class="dropdown header-profile">
+                <a class="nav-link" href="javascript:void(0);" role="button" data-bs-toggle="dropdown">
+                    <img src="/images/avatar/1.png" width="20" alt="">
+                    <div class="header-info ms-3">
+                        <span class="font-w600 ">Hello,<b>Seynabou</b></span>
+                        <small class="text-end font-w400">admin@gmail.com</small>
+                    </div>
+                </a>
+                <div class="dropdown-menu dropdown-menu-end">
+                    <a href="app-profile.html" class="dropdown-item ai-icon">
+                        <svg id="icon-user1" xmlns="http://www.w3.org/2000/svg" class="text-primary" width="18"
+                            height="18" viewbox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                            <circle cx="12" cy="7" r="4"></circle>
+                        </svg>
+                        <span class="ms-2">Profile </span>
+                    </a>
+                    <a href="email-inbox.html" class="dropdown-item ai-icon">
+                        <svg id="icon-inbox" xmlns="http://www.w3.org/2000/svg" class="text-success" width="18"
+                            height="18" viewbox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z">
+                            </path>
+                            <polyline points="22,6 12,13 2,6"></polyline>
+                        </svg>
+                        <span class="ms-2">Inbox </span>
+                    </a>
+                    <a href="page-error-404.html" class="dropdown-item ai-icon">
+                        <svg id="icon-logout" xmlns="http://www.w3.org/2000/svg" class="text-danger" width="18"
+                            height="18" viewbox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
+                            <polyline points="16 17 21 12 16 7"></polyline>
+                            <line x1="21" y1="12" x2="9" y2="12"></line>
+                        </svg>
+                        <span class="ms-2">Logout </span>
+                    </a>
+                </div>
+            </li>
+            <li><a href="widget-basic.html" class="ai-icon" aria-expanded="false">
+                    <i class="flaticon-013-checkmark"></i>
+                    <span class="nav-text">Dashbord</span>
+                </a>
+            </li>
+            <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                    <i class="flaticon-050-info"></i>
+                    <span class="nav-text">Users</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="{{ route('admin.users.create') }}">Ajout Users</a></li>
+                    <li><a href="{{ route('admin.users.index') }}">Liste Users</a></li>
+                </ul>
+            </li>
+
+            <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                    <i class="flaticon-086-star"></i>
+                    <span class="nav-text">Roles</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="{{ route('admin.roles.create') }}">Ajout Role</a></li>
+                    <li><a href="{{ route('admin.roles.index') }}">Liste Role</a></li>
+                </ul>
+            </li>
+            <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                    <i class="flaticon-045-heart"></i>
+                    <span class="nav-text">Profile</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="{{ route('admin.profils.create') }}">Ajout Profile</a></li>
+                    <li><a href="{{ route('admin.profils.index') }}">Liste Profile</a></li>
+
+                </ul>
+            </li>
+            <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                    <i class="flaticon-072-printer"></i>
+                    <span class="nav-text">Permissions</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="{{ route('admin.permissions.create') }}">Ajout Permissions</a></li>
+                    <li><a href="{{ route('admin.permissions.index') }}">Liste Permissions</a></li>
+                </ul>
+            </li>
+
+
+            <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                    <i class="flaticon-022-copy"></i>
+                    <span class="nav-text">{{ Auth::user()->name }}</span>
+                </a>
+                <ul aria-expanded="false">
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        <x-responsive-nav-link :href="route('logout')"
+                            onclick="event.preventDefault();
+                            this.closest('form').submit();">
+                            {{ __('Log Out') }}
+                        </x-responsive-nav-link>
+                    </form>
+                </ul>
+            </li>
+        </ul>
+
+
+    </div>
+
+</div>
